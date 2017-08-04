511e26e2d3b2cd8360b6607c1f5617e48d53e687
pablues <pashka3k@gmail.com>
2017-08-05 00:22:23 +0200

    JavaScript-Basics-Base-01

--- js/script.js
+++ js/script.js
@@ -1,74 +1,7 @@
-/**
- * Created by pablues on 26.07.2017.
- */
 
 
 
- var myData = ["A", "B", "C", "D", "E", "F", "G"];
- var my2ndData = [];
 
- function printmyData() {
-     console.log(myData);
- }
- function printmy2ndData() {
-     console.log(my2ndData);
- }
- function push2ndArray() {
-     var eingabe = prompt("Gebe den Wert ein");
-     my2ndData.push(eingabe);
-     printmy2ndData()
- }
- document.querySelector("span").onclick = function () {
-    console.log("Hi ich bin deine Console")
- }
-
- function addDivs(){
- var u = d3.select("#content")
- .selectAll("div")
- .data(myData);
-
- u.enter()
- .append("div")
- .merge(u)
- .text(function (d) {
- return d;
- });
- u.exit().remove();
-
- }
-
- function removeDivs() {
- d3.select("#content")
- .selectAll("div")
- .data(my2ndData)
- .exit()
- .remove();
-
- }
-
- var cont = [];
-
-document.querySelector('#hinzu').onclick = function () {
-  var c = prompt("Gebe ein");
-  cont.push(c);
-  console.log(cont);
-  c = d3.select("#content").selectAll("div").data(cont);
-    c.enter()
-        .append("div")
-        .merge(c)
-        .text(function (d) {
-            return d;
-        })
-        .transition()
-        .duration(750)
-        .style("opacity", 1)
-        .duration(1200)
-        .style("transform", "translateX(10px)")
-        .style("background-color","coral")
-        .style("color", "white")
-
-
-};
 
 
 
